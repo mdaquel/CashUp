77e57e6361f5a1526fcd9ebf34d4e0ffa8a34e44
mdaquel <mdaquel@gmail.com>
2015-09-24 08:48:55 +1000

    Domain: Application
    Updated ApplicationRegistration. Added required domain contract
    references.

--- src/Domain/Applications/Handlers/ApplicationRegistration/ApplicationRegistration.cs
+++ src/Domain/Applications/Handlers/ApplicationRegistration/ApplicationRegistration.cs
@@ -1,6 +1,13 @@
-﻿namespace ApplicationRegistration
+﻿using CashUp.Domain.Applications.Commands;
+using Domain.Contracts;
+
+namespace ApplicationRegistration
 {
-    public class ApplicationRegistration
+    public class ApplicationRegistration : IHandleMessage<PlaceApplication>
     {
+        public void Handle(PlaceApplication message)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
